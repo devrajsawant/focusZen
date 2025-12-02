111daadbaafd06774fe5f30a065902336e96cc43
Devraj <devrajsawant6@gmail.com>
2025-12-02 23:08:40 +0530

    change the head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,31 @@
 // app/layout.tsx
-import Sidebar from '@/components/Sidebar'
-import './globals.css'
-import { ReactNode } from 'react'
-import Head from 'next/head'
+import Sidebar from "@/components/Sidebar";
+import "./globals.css";
+import { ReactNode } from "react";
 
 export const metadata = {
-  title: 'FocusZen-Dashboard',
-  description: 'Plan your day and reach your goals',
-}
+  title: "FocusZen-Dashboard",
+  description: "Plan your day and reach your goals",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#ffffff" />
         <link rel="apple-touch-icon" href="/FocusZenLogo2.png" />
-      </Head>
-      <body className="flex min-h-screen bg-[#FAF4E4]"  cz-shortcut-listen="true">
+        <link rel="icon" href="/FocusZenLogo2.png" />
+        {/* optional: explicit sizes */}
+        <link rel="icon" sizes="192x192" href="/FocusZenLogo2.png" />
+        <link rel="icon" sizes="512x512" href="/FocusZenLogo2.png" />
+      </head>
+      <body className="flex min-h-screen bg-[#FAF4E4]">
         <Sidebar />
-        <main className="flex-1 mb-20 mt-20 md:mb-0 md:mt-0 bg-[#FAF4E4]" >{children}</main>
+        <main className="flex-1 mb-20 mt-20 md:mb-0 md:mt-0 bg-[#FAF4E4]">
+          {children}
+        </main>
       </body>
     </html>
-  )
+  );
 }
